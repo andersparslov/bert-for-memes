7e609507d294de2701824407d585cdb71cca01c6
anders parslov <s164210@student.dtu.com>
2022-01-07 11:35:15 +0100

    Model

--- src/models/Model.py
+++ src/models/Model.py
@@ -5,7 +5,7 @@ from torch import nn, argmax
 
 class MemeModel(nn.Module):
 
-    def __init__(self, device, lm=None, num_labels=2):
+    def __init__(self, device, num_labels=2):
         self.device = device
         if "distilbert-base-uncased" in os.listdir("models"):
             model_path = "models\\distilbert-base-uncased"
@@ -15,10 +15,10 @@ class MemeModel(nn.Module):
         self.mod = mod_fn.from_pretrained(model_path, num_labels=num_labels).to(device)
         self.tokenizer = DistilBertTokenizer.from_pretrained(model_path)    
 
-    def forward(self, item, labels):
+    def forward(self, item):
         y = item['labels'].to(self.device)
         x_doc = item['input_ids'].to(self.device)
-        y_pred = self.mod(x_doc, labels=labels)
+        y_pred = self.mod(x_doc, labels=y)
         loss = y_pred.loss
         y_pred = argmax(y_pred.logits, axis=1)
         return y_pred, loss
