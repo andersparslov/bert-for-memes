dcfb954a9b5e570d492c391cdc632a991657f78c
anders parslov <s164210@student.dtu.com>
2022-01-07 11:20:30 +0100

    Model class

---
+++ src/models/Model.py
@@ -0,0 +1,24 @@
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+import os
+from torch import nn, argmax
+
+
+class MemeModel(nn.Module):
+
+    def __init__(self, device, lm=None, num_labels=2):
+        self.device = device
+        if "distilbert-base-uncased" in os.listdir("models"):
+            model_path = "models\\distilbert-base-uncased"
+        else:
+            model_path = "distilbert-base-uncased"
+        mod_fn = DistilBertForSequenceClassification 
+        self.mod = mod_fn.from_pretrained(model_path, num_labels=num_labels).to(device)
+        self.tokenizer = DistilBertTokenizer.from_pretrained(model_path)    
+
+    def forward(self, item, labels):
+        y = item['labels'].to(self.device)
+        x_doc = item['input_ids'].to(self.device)
+        y_pred = self.mod(x_doc, labels=labels)
+        loss = y_pred.loss
+        y_pred = argmax(y_pred.logits, axis=1)
+        return y_pred, loss
