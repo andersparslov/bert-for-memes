d948a4c2d2415737c6348280aa62af3166b726a0
anders parslov <s164210@student.dtu.com>
2022-01-18 09:49:27 +0100

    Predict script

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,16 +1,21 @@
 from Model import MemeModel
+from transformers import DistilBertTokenizer
 import torch
 
 
-def predict_model(item):
-    device = torch.device("cuda")
-    model = MemeModel()
-    # TO-DO : Checkpoint model 
+def predict_model():
+    tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+    item = tokenizer("Hello testing this meme",padding='max_length', 
+              max_length = 512, truncation=True,
+              return_tensors="pt")
+    
+    device = torch.device("cpu")
+    model = MemeModel(1, device=device, num_labels=4)    
     with torch.no_grad():
-        y = item['labels'].to(device)
-        x_doc = item['input_ids'].to(device)
-        y_pred = model(x_doc.unsqueeze(0), labels=y)
-        y_pred = torch.argmax(y_pred.logits, axis=1)
+        y_pred, _ = model(item['input_ids'], 
+                       item['attention_mask'], 
+                       labels=None)
+        y_pred = torch.argmax(y_pred, axis=1)
         
     return y_pred
 
