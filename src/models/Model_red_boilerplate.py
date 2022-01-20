d4db5a0a1cb2b555ee0eba23c256a31937a251f2
s194649 <s194649@student.dtu.dk>
2022-01-20 11:51:45 +0100

    removed redundant imports and updated requirements using nbpipreqs

--- src/models/Model_red_boilerplate.py
+++ src/models/Model_red_boilerplate.py
@@ -1,10 +1,8 @@
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+from transformers import DistilBertForSequenceClassification
 import os
-import torch.nn as nn
 from pytorch_lightning import LightningModule
 import torch
-from torch import nn, optim
-import matplotlib.pyplot as plt
+from torch import optim
 
 from pathlib import Path
 PROJECT_PATH = Path(__file__).resolve().parents[2]
